ca7ecf580f197f268aae42421899ee8b5a493cd5
MauroRaya <maurorayabr@gmail.com>
2026-05-05 22:02:05 -0300

    feat: adiciona multiplos marcadores baseado na api

--- src/features/plants/pages/MapPage.jsx
+++ src/features/plants/pages/MapPage.jsx
@@ -1,10 +1,19 @@
 import { ArrowLeft } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useMyPlants } from '../hooks/usePlants';
 import styles from './MapPage.module.css';
 
 export default function MapPage() {
   const navigate = useNavigate();
+	const { data, isLoading } = useMyPlants(1);
+
+	const plants = data?.itens ?? [];
+
+	const plantsWithLocation = plants.filter((p) => 
+		p.latitude &&
+		p.longitude
+	);
 
   const defaultPosition = {
     lat: -23.9700,
@@ -19,17 +28,30 @@ export default function MapPage() {
       </button>
 
       <div className={styles.mapWrapper}>
-        <MapContainer
-          center={defaultPosition}
-          zoom={14}
-          scrollWheelZoom={true}
-          className={styles.map}
-        >
-          <TileLayer
-            attribution="&copy; OpenStreetMap contributors"
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
+				<MapContainer
+					center={defaultPosition}
+					zoom={13}
+					scrollWheelZoom={true}
+					className={styles.map}
+				>
+				<TileLayer
+					attribution="&copy; OpenStreetMap contributors"
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+
+				{plantsWithLocation.map((plant) => (
+					<Marker
+						key={plant.id}
+						position={[plant.latitude, plant.longitude]}
+					>
+					<Popup>
+					<strong>{plant.nomeComum || plant.nomeCientifico}</strong>
+					<br />
+						{plant.familia && <span>Família: {plant.familia}</span>}
+					</Popup>
+					</Marker>
+				))}
+				</MapContainer>
       </div>
     </div>
   );
